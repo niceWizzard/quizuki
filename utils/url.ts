a8a3af3fb8fc1aa9fbbd0cffdd5383692f26b2e2
RIchard Manansala <richard232manansala@gmail.com>
2025-07-27 18:31:33 +0800

    Made the regex accept quizizz domain name as well

--- utils/url.ts
+++ utils/url.ts
@@ -1,12 +1,10 @@
 export function isValidWaygroundUrl(url: string): boolean {
-    const regex = /^(https?:\/\/)?(www\.)?wayground\.com\/(?:join\/)?quiz\/([a-f0-9]{24})(\/[^\s]*)?(\?[^\s]*)?(#[^\s]*)?$/;
+    const regex = /^(https?:\/\/)?(www\.)?(wayground\.com|quizizz\.com)\/(?:join\/)?quiz\/([a-f0-9]{24})(\/[^\s]*)?(\?[^\s]*)?(#[^\s]*)?$/;
     return regex.test(url);
 }
 
-
-
 export function extractWaygroundQuizId(url: string): string | null {
-    const regex = /wayground\.com\/(?:join\/)?quiz\/([a-f0-9]{24})/;
+    const regex = /(wayground\.com|quizizz\.com)\/(?:join\/)?quiz\/([a-f0-9]{24})/;
     const match = url.match(regex);
-    return match ? match[1] : null;
+    return match ? match[2] : null;
 }
