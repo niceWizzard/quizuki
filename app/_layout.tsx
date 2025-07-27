6d15428ff51b64bd7ad169e305ab7409f6724a0d
RIchard Manansala <richard232manansala@gmail.com>
2025-07-27 14:33:01 +0800

    made create screen transition to be fade from bottom

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from "expo-router";
 import DrizzleProvider from "@/providers/DrizzleProvider";
 import ReactNativePaperProvider from "@/providers/ReactNativePaperProvider";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <ReactNativePaperProvider>
@@ -16,6 +16,7 @@ export default function RootLayout() {
           name="quiz/create"
           options={{
             headerTitle: "Create Quiz",
+            animation: 'fade_from_bottom',
           }}
         />
       </Stack>
