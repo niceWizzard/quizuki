f35974cec4ef9553f02858030f66cdde865839a9
RIchard Manansala <richard232manansala@gmail.com>
2025-07-27 22:30:33 +0800

    Added insertion feature in preview

--- app/quiz/[id]/preview.tsx
+++ app/quiz/[id]/preview.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {ActivityIndicator, FlatList, ScrollView, View} from "react-native";
-import {Card, Text} from "react-native-paper";
-import {useLocalSearchParams} from "expo-router";
+import {Card, FAB, Text} from "react-native-paper";
+import {router, useLocalSearchParams} from "expo-router";
 import {useQuery} from "@tanstack/react-query";
 import {Image} from "expo-image";
 import {QuizSchema} from "@/utils/fetchSchema";
+import {useDrizzleStore} from "@/store/useDrizzleStore";
+import {quizTable} from "@/db/schema";
 
 const PreviewScreen = () => {
     const { id } = useLocalSearchParams();
+    const drizzle = useDrizzleStore(v => v.drizzle!);
 
     const {data, isLoading, error} = useQuery({
         queryKey: ['preview', id],
@@ -37,38 +40,57 @@ const PreviewScreen = () => {
 
     const quiz = data!;
 
+    async function saveQuizToDb() {
+        await drizzle.insert(quizTable).values({
+            name: data!.name,
+            waygroundId: data!.id,
+        })
+        router.back();
+    }
+
     return (
-        <FlatList
-            data={quiz.questions}
-            keyExtractor={(item) => item._id}
-            contentContainerStyle={{ paddingHorizontal: 16, gap: 8, paddingBottom: 32, }}
-            ListHeaderComponent={
-                <>
-                    {data!.image && (
-                        <Image
-                            source={{ uri: data!.image }}
-                            style={{ width: '100%', height: 200 }}
-                            contentFit="cover"
-                        />
-                    )}
-                    <Text variant="titleLarge">{quiz.name}</Text>
-                    <Text variant="bodySmall">{quiz.createdBy}</Text>
-                    <Text variant="titleMedium">Questions</Text>
-                </>
-            }
-            renderItem={({ item }) => (
-                <Card>
-                    <Card.Title title={item.structure.query.text} />
-                    <Card.Content>
-                        {item.structure.options.map((option, index) => (
-                            <Text key={option.id}>
-                                {option.text}
-                            </Text>
-                        ))}
-                    </Card.Content>
-                </Card>
-            )}
-        />
+        <>
+            <FlatList
+                data={quiz.questions}
+                keyExtractor={(item) => item._id}
+                contentContainerStyle={{ paddingHorizontal: 16, gap: 8, paddingBottom: 32, }}
+                ListHeaderComponent={
+                    <>
+                        {data!.image && (
+                            <Image
+                                source={{ uri: data!.image }}
+                                style={{ width: '100%', height: 200 }}
+                                contentFit="cover"
+                            />
+                        )}
+                        <Text variant="titleLarge">{quiz.name}</Text>
+                        <Text variant="bodySmall">{quiz.createdBy}</Text>
+                        <Text variant="titleMedium">Questions</Text>
+                    </>
+                }
+                renderItem={({ item }) => (
+                    <Card>
+                        <Card.Title title={item.structure.query.text} />
+                        <Card.Content>
+                            {item.structure.options.map((option, index) => (
+                                <Text key={option.id}>
+                                    {option.text}
+                                </Text>
+                            ))}
+                        </Card.Content>
+                    </Card>
+                )}
+            />
+            <FAB
+                label="Save Quiz"
+                style={{
+                    position: 'absolute',
+                    bottom: 16,
+                    right: 16,
+                }}
+                onPress={saveQuizToDb}
+            />
+        </>
     );
 };
 
