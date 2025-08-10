6ec080dd12d0a8ab861d09ecaf1a8c11e2a381d5
RIchard Manansala <richard232manansala@gmail.com>
2025-08-10 11:04:53 +0800

    Ran drizzle kit generate

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_sharp_sentry.sql';
 import m0001 from './0001_free_charles_xavier.sql';
+import m0002 from './0002_aberrant_omega_flight.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
