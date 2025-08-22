d4ba5a9e0b995b63c234a482b87aa4df45404fd3
RIchard Manansala <richard232manansala@gmail.com>
2025-08-22 20:19:39 +0800

    Displayed the question in a card as well as the image if available

--- src/app/quiz/[id]/play/[question]/index.tsx
+++ src/app/quiz/[id]/play/[question]/index.tsx
@@ -3,7 +3,8 @@ import { Question } from '@/types/db';
 import { router, useLocalSearchParams } from 'expo-router';
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import {Button, Card, Text} from 'react-native-paper';
+import {Image} from "expo-image";
 
 const PlayIndexScreen = () => {
   const { id, question } = useLocalSearchParams();
@@ -31,13 +32,25 @@ const PlayIndexScreen = () => {
 
   return (
     <View style={{
-      padding: 8,
+      paddingHorizontal: 24,
       justifyContent: 'center',
       flex: 1,
       alignItems: 'center',
     }}>
-      <Text>{questionIndex+1} out of {activePlay.questionOrder.length}</Text>
-      <Text>{data.text}</Text>
+      <Card style={{width: '100%'}}>
+        <Card.Content style={{gap: 16}}>
+          <Text variant="bodySmall" style={{textAlign: 'center'}}>
+            {questionIndex+1} out of {activePlay.questionOrder.length}
+          </Text>
+          <Text>{data.text}</Text>
+          {data.images.map((path) => (<Image
+              key={path}
+              source={{uri: path}}
+              style={{width: '100%', minHeight: 256, height: 'auto'}}
+              contentFit={'contain'}
+          />))}
+        </Card.Content>
+      </Card>
       <Button onPress={() => router.replace({
         pathname: '/quiz/[id]/play/[question]',
         params: {
