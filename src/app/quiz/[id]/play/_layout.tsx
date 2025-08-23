841fb127c03fb247fc8f75b373f8b26e7a064422
RIchard Manansala <richard232manansala@gmail.com>
2025-08-23 09:17:40 +0800

    Changed useEffect to route pop listener

--- src/app/quiz/[id]/play/_layout.tsx
+++ src/app/quiz/[id]/play/_layout.tsx
@@ -1,17 +1,39 @@
-import {Stack, Tabs} from "expo-router";
+import {Stack, useNavigation} from "expo-router";
 import {useEffect} from "react";
 import {useRepositoryStore} from "@/store/useRepositoryStore";
+import {Alert} from "react-native";
 
 
 export default function PlayQuestionLayout() {
 
     const playRepo = useRepositoryStore(v => v.play!);
+    const navigation = useNavigation();
 
     useEffect(() => {
-        return () => {
-            playRepo.clearPlay();
-        }
-    }, [playRepo]);
+        return navigation.addListener('beforeRemove', (e) => {
+            if(e.data.action.type === 'POP') {
+                e.preventDefault();
+                Alert.alert(
+                    'Quiz playing?',
+                    'Your session will be saved.',
+                    [
+                        {
+                            text: "Cancel", style: 'cancel', onPress: () => {
+                            }
+                        },
+                        {
+                            text: 'Leave',
+                            style: 'destructive',
+                            onPress: () => {
+                                playRepo.clearPlay();
+                                navigation.dispatch(e.data.action);
+                            },
+                        },
+                    ]
+                );
+            }
+        });
+    }, [navigation, playRepo]);
   return <Stack>
       <Stack.Screen
           name="[question]/index"
